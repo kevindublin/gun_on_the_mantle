d017808b2a28d03fc60558b0372fa10e0d0ab5e7
Vin Dublin <kevdublin@gmail.com>
2019-03-09 09:11:35 -0800

    somewhat functional

--- urls.py
+++ urls.py
@@ -4,13 +4,17 @@ import views
 
 # In this example, we've separated out the views.py into a new file
 urlpatterns = [
-    path('', views.index),
-    path('about-me', views.about_me),
-    path('github-api-example', views.github_api_example),
+    path('', views.about),
+    path('about', views.about),
+    path('rules', views.rules),
+    path('play', views.play),
+    path('character-cards', views.character_cards),
+    path('sample-setup', views.under_construction),
+    path('gameplay-details', views.gameplay_details),
+    path('contact', views.contact),
 ]
 
 # Boilerplate to include static files
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
--- views.py
+++ views.py
@@ -3,25 +3,36 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def index(request):
-    # This is similar to ones we have done before. Instead of keeping
-    # the HTML / template in a separate directory, we just reply with
-    # the HTML embedded here.
-    return HttpResponse('''
-        <h1>Welcome to my home page!</h1>
-        <a href="/about-me">About me</a> <br />
-        <a href="/github-api-example">See my GitHub contributions</a> <br />
-    ''')
+character_cards = [{
+    'card_title': 'Hacker',
+    'card_skill_1': 'Steal: Take 2 coins from another player',
+    'card_skill_2': 'Blocks inspection',
+    'img_src': '../static/img/hacker.svg'},
+    {'card_title': 'Inspector',
+        'card_skill_1': 'Inspect: Look at another card, then return it',
+        'card_skill_2': 'Blocks stealing',
+        'img_src': '../static/img/inspector.svg'},
+    {'card_title': 'Athlete',
+        'card_skill_1': 'Compete: Take 3 coins from the stash',
+        'card_skill_2': 'Blocks other competition',
+        'img_src': '../static/img/athlete.svg'},
+    {'card_title': 'Hunter',
+        'card_skill_1': 'Target: Pay 7 coins to take the gun from the mantle',
+        'card_skill_2': 'Skill cannot be replicated',
+        'img_src': '../static/img/hunter.svg'},
+    {'card_title': 'Scientist',
+        'card_skill_1': 'Replicate: Repeat the previous skill used',
+        'card_skill_2': 'Blocks exchanges',
+        'img_src': '../static/img/scientist.svg'},
+    {'card_title': 'Teacher',
+        'card_skill_1': 'Exchange: Switch this card with any player',
+        'card_skill_2': 'Blocks Targeting by Hunter',
+        'img_src': '../static/img/teacher.svg'}]
 
 
-def about_me(request):
-    # Django comes with a "shortcut" function called "render", that
-    # lets us read in HTML template files in separate directories to
-    # keep our code better organized.
-    context = {
-        
-    }
-    return render(request, 'about_me.html', context)
+def about(request):
+    context = {}
+    return render(request, "about.html", context)
 
 
 def github_api_example(request):
@@ -32,3 +43,35 @@ def github_api_example(request):
         'github_repos': repos,
     }
     return render(request, 'github.html', context)
+
+
+def under_construction(request):
+
+    return HttpResponse('''
+        This page is currently being built. Sorry for the inconvenience!
+    ''')
+
+
+def rules(request):
+    context = {}
+    return render(request, "rules.html", context)
+
+
+def play(request):
+    context = {}
+    return render(request, "play.html", context)
+
+
+def character_cards(request):
+    context = {character_cards}
+    return render(request, "character_cards.html", context)
+
+
+def gameplay_details(request):
+    context = {}
+    return render(request, "gameplay_details.html", context)
+
+
+def contact(request):
+
+    return
